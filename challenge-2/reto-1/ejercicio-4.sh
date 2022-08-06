a3a886697e5ae09f4e47a2c5752a328678aa64a2
yasledesma <yaslesd@gmail.com>
2022-08-05 21:41:27 -0300

    Se agrega ejercicio 4 del reto 1

--- challenge-2/reto-1/ejercicio-4.sh
+++ challenge-2/reto-1/ejercicio-4.sh
@@ -2,4 +2,13 @@
 # Crea un script de bash que descargue el contenido de una página web a un fichero. por ejemplo "https://es.wikipedia.org/wiki/DevOps" Una vez descargado el fichero, que busque en el mismo una palabra dada (esta se pasará por parametro) y muestre por pantalla el número de linea donde aparece.
 
 #!/usr/bin/env bash
-curl https://es.wikipedia.org/wiki/DevOps > download.html
+if [ -z download.html ]
+then
+    curl https://es.wikipedia.org/wiki/DevOps > download.html
+else
+    echo "Ya existe el archivo"
+fi
+
+isInFile=$(cat download.html | grep -i -m 1 -n "devops")
+
+echo "$isInFile"
